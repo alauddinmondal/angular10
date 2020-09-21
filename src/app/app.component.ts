4131718a71c5a9eff14f5bd865eb8cce574b1fdd
alauddinmondal <arshialauddin@gmail.com>
2020-09-21 18:31:03 +0400

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  mydata = [
+    {
+    name:'Alauddin',
+    age:'40',
+    address:'Paharpur'
+  },
+  {
+    name:'Romjan',
+    age:'30',
+    address:'Patrasayer'
+  },
+  {
+    name:'Najir',
+    age:'25',
+    address:'Hyderabad'
+  }
+]
 }
